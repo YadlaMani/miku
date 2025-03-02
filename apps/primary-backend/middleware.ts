c4c0396d412ca378ee48d28a88598f7809b79c56
YadlaMani <yadlamaniymn2005@gmail.com>
2025-03-02 19:07:15 +0530

    added primary backend with middle and clerk auth

--- apps/primary-backend/middleware.ts
+++ apps/primary-backend/middleware.ts
@@ -1,14 +1,28 @@
 import type { Request, Response, NextFunction } from "express";
-
+import jwt from "jsonwebtoken";
 export function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({
       message: "Unauthorized",
     });
   }
+  const decoded = jwt.verify(token, process.env.JWT_PUBLIC_KEY!);
+  if (!decoded) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  const userId = (decoded as any).payload.sub;
+  if (!userId) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  req.userId = userId;
+  next();
 }
